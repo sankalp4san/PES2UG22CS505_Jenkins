7c25c730829f10b96dc2ce4436b278460ec93b1b
Sankalp <sankalp4san@gmail.com>
2025-03-12 10:21:31 +0530

    updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, PES2UG22CS505\n";
+  cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Task 4\n";
   return 0;
 }
